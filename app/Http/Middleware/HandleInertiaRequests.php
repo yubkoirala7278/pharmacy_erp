5eca65611157e51472ba0b3e0fc9455a6e95b66f
unknown <yubrajkoirala7278@gmail.com>
2026-05-11 14:34:11 +0545

    project setup completed

---
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Inertia\Middleware;
+use Illuminate\Http\Request;
+
+class HandleInertiaRequests extends Middleware
+{
+    /**
+     * The root template that's loaded on the first page visit.
+     */
+    protected $rootView = 'app';
+    /**
+     * Determine the current asset version.
+     */
+    public function version(Request $request): string|null
+    {
+        return parent::version($request);
+    }
+
+    /**
+     * Define the props that are shared by default.
+     */
+    public function share(Request $request): array
+    {
+        return array_merge(parent::share($request), [
+            'auth' => [
+                'user' => $request->user() ? [
+                    'id' => $request->user()->id,
+                    'name' => $request->user()->name,
+                ] : null,
+            ],
+        ]);
+    }
+}
